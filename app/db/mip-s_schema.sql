f2953ddc65bee3907242c2374d418754be5cc014
Duarte Assunção <up202208319@up.pt>
2025-10-29 20:30:58 +0000

    Database schema

--- app/db/mip-s_schema.sql
+++ app/db/mip-s_schema.sql
@@ -1,27 +1,34 @@
--- Drop existing tables (safe reset)
-DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
+-- Drop existing Tables (safe reset)
+DROP TABLE IF EXISTS award CASCADE;
 
--- Create tables
-CREATE TABLE products (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  description TEXT,
-  image_url TEXT
+-- Create Enums
+CREATE TYPE AWARD_TYPE AS ENUM('best', 'most_sold', 'revelation');
+
+-- Create Tables
+CREATE TABLE award (
+    id SERIAL PRIMARY KEY,
+    year SMALLINT NOT NULL DEFAULT date_part('year', NOW()),
+    award_type AWARD_TYPE NOT NULL,
+    product_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL
 );
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(100) NOT NULL
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    rating NUMERIC(1, 0) NOT NULL CHECK(rating >= 1) CHECK(rating <=5),
+    comment TEXT DEFAULT NULL,
+    created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES users(id),
-  total DECIMAL(10,2),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE used_points (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    points INTEGER NOT NULL
+);
+
+CREATE TABLE loyalty (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    points INTEGER NOT NULL
 );
