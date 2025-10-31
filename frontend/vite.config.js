c15f5ffe133771acf55bdb6ff94350cc0e6d3151
Hugo Barbosa <hugobarbosa.H0gy@gmail.com>
2025-10-31 19:44:40 +0000

    Containers Work fine!
    Co-authored-by: DuarteSAssuncao <up202208319@up.pt>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,12 +10,9 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:3000',  // backend during local dev
-    //     changeOrigin: true,
-    //     secure: false
-    //   }
-    // }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173
   }
 })
--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 # Start Containers:
 docker compose up
-
-# Create and populate database:
-docker exec -it postgres_db psql -U postgres -d madeinportugal -f /docker-entrypoint-initdb.d/mip-s_schema.sql
-docker exec -it postgres_db psql -U postgres -d madeinportugal -f /docker-entrypoint-initdb.d/populate.sql
