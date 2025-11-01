e567cb570fe36e783d7ffbb2024abe9c18282aed
Hugo Barbosa <hugobarbosa.H0gy@gmail.com>
2025-11-01 02:39:41 +0000

    deleted code that was merged into main by mistake

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export const BACKEND = 'http://localhost:3000';
-export const API_BASE = BACKEND + '/api';
-
-export async function fetchProducts() {
-  const res = await fetch(`${API_BASE}/products`);
-  if (!res.ok) throw new Error('Failed to load products');
-  return res.json();
-}
